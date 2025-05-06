23bc86d820779fc19ed3c9b2729f1780f75e01d3
abengkris <abengkrischannel@gmail.com>
2025-05-07 00:41:22 +0700

    add sosmed

--- src/consts.ts
+++ src/consts.ts
@@ -17,16 +17,28 @@ export const NAV_LINKS: SocialLink[] = [
     label: 'blog',
   },
   {
-    href: '/authors',
-    label: 'authors',
+    href: '/karya',
+    label: 'karya',
+  },
+  {
+    href: '/support',
+    label: 'dukung',
   },
   {
     href: '/about',
-    label: 'about',
+    label: 'tentang',
   },
 ]
 
 export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://instagram.com/abengkriss',
+    label: 'Instagram',
+  },
+  {
+    href: 'https://www.threads.net/@abengkriss',
+    label: 'Threads',
+  },
   {
     href: 'https://github.com/abengkris',
     label: 'GitHub',
@@ -46,6 +58,8 @@ export const SOCIAL_LINKS: SocialLink[] = [
 ]
 
 export const ICON_MAP: IconMap = {
+  Threads: 'threads',
+  Instagram: 'lucide:instagram',
   Website: 'lucide:globe',
   GitHub: 'lucide:github',
   LinkedIn: 'lucide:linkedin',
